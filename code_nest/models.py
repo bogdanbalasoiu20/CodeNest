844431e4eccd7151582f7ac4407edf6e66dd6202
bogdan <balasoiu.bogdan@gmail.com>
2025-03-17 22:57:03 +0200

    am adaugat un tabel de test

--- code_nest/models.py
+++ code_nest/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TestTable(models.Model):
+    field1=models.CharField(max_length=255)
+    field2=models.IntegerField()
+    field3=models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.field1
