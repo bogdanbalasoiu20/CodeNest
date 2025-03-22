5acc1d562457607612c0cd7afc05eac84d3fa362
bogdan <balasoiu.bogdan@gmail.com>
2025-03-23 01:30:21 +0200

    optiune de resetare a parolei

--- code_nest/urls.py
+++ code_nest/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -9,4 +8,10 @@ urlpatterns = [
     path("login/", views.custom_login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
+    
+    # Password reset views:
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
